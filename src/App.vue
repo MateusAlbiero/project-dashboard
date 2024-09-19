c5793a0b7168354fa571ef3dd669ab00f09d5e77
Mateus Albiero <mateusalbiero1@gmail.com>
2024-09-19 17:46:01 -0300

    :sparkles: feat: Integração da listagem de tarefas do GitHub

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,25 @@
 </template>
 
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { apiGitHub } from './services/githubService';
+// import { apiClickUp } from './services/clickupService';
+
+// const clickupTasks = ref([]);
+const githubTasks = ref([]);
+
+onMounted(async () => {
+  try {
+    const githubResponse = await apiGitHub.get(''); //GitHub
+    githubTasks.value = githubResponse.data;
+
+    // const clickupResponse = await apiClickUp.get(''); //ClickUp
+    // clickupTasks.value = clickupResponse.data;
+  } catch (error) {
+    console.error('Erro ao retornar os dados da API: ', error);
+  }
+});
+
 defineOptions({
   name: 'App'
 });
