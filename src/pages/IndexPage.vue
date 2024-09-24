45890d97a91b5eb927e5731906d857ec38173cd3
Mateus Albiero <mateusalbiero1@gmail.com>
2024-09-24 15:45:13 -0300

    :sparkles: feat: adicionado dados da organização

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,8 +1,6 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    
-    Dados da organização: {{ githubOrg }}
-
+  <q-page class="row justify-evenly">
+    Listagem de tarefas da organização {{ githubOrg?.name }}
   </q-page>
 </template>
 
@@ -10,12 +8,27 @@
 import { onMounted, ref } from 'vue';
 import { apiGitHub } from 'src/services/githubService';
 
-const githubOrg = ref([]);
+interface GitHubOrg {
+  name: string;
+}
+
+interface GitHubRepos {
+  name: string;
+  login: string;
+  id: number;
+}
+
+const githubOrg = ref<GitHubOrg | null>(null);
+const githubRepos = ref<GitHubRepos | null>(null);
+
 
 onMounted(async () => {
   try {
     const gitOrgs = await apiGitHub.get('/orgs/sgbrsist');
     githubOrg.value = gitOrgs.data;
+
+    const gitRepos = await apiGitHub.get('/orgs/sgbrsist');
+    githubRepos.value = gitRepos.data;
   } catch (error) {
     console.error('Erro ao retornar os dados da API: ', error);
   }
