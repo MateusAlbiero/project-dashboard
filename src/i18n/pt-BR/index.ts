81eed8738f4d15702c1257b1e927def87e4eda9e
Mateus Albiero <mateusalbiero1@gmail.com>
2024-10-10 17:27:22 -0300

    :sparkles: i18n e att vue

---
+++ src/i18n/pt-BR/index.ts
@@ -0,0 +1,11 @@
+export default {
+    priority: {
+        urgent: 'Urgente',
+        pending: 'Pendente',
+        completed: 'Concluído',
+        high: 'Alta',
+        low: 'Baixa',
+        normal: 'Normal',
+
+    }
+}
