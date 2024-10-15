2025736499c10c86f6d3b7307dc0cb97fe55ed24
Mateus Albiero <mateusalbiero1@gmail.com>
2024-10-15 09:39:51 -0300

    :fire: fix: ajustes nos detalhes

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -226,65 +226,70 @@
     <q-dialog
       v-model="isDialogOpen"
       full-height
-      full-width
       transition-show="scale"
       transition-hide="scale"
       class="mt-1"
       :inert="!isDialogOpen"
     >
 
-      <q-card class="q-pa-md">
+      <q-card class="q-pa-md card">
         <q-card-section class="row items-center q-pb-none">
           <div class="text-h6 text-grey-8">Detalhes da tarefa</div>
           <q-space />
           <q-btn icon="close" flat round dense @click="isDialogOpen = false" />
         </q-card-section>
 
-        <q-card-section class="q-pt-none mr-5 ml-5">
-          <h5>{{ selectedTask.name }}</h5>
+        <q-card-section class="q-pt-none mr-4 ml-4">
+          <div class="text-h5 mb-2 mt-4">{{ selectedTask.name }}</div>
           <div class="flex-jb">
             <div class="flex-js">
               <section class="details-task">
                 <strong class="mb-05"><q-icon name="description" class="mr-05"/>Protocolo</strong>
                 <strong class="mb-05"><q-icon name="info" class="mr-05"/>Status</strong>
                 <strong class="mb-05"><q-icon name="label" class="mr-05"/>Etiquetas</strong>
+                <strong class="mb-05"><q-icon name="phone_in_talk" class="mr-05"/>Contato parceiro</strong>
               </section>
               <section>
                 <div>{{ selectedTask.custom_id ? selectedTask.custom_id : 'Sem protocolo' }}</div>
-                  <div>
+                <div>
+                  <q-chip
+                    size="12px"
+                    :class="{
+                      'bg-orange-8 text-white':
+                        selectedTask.status?.id === 'sc901303007954_qiBEkmhZ',
+                      'bg-yellow-8 text-white':
+                        selectedTask.status?.id === 'sc901303007954_VhnEd1ez',
+                      'bg-blue-8 text-white':
+                        selectedTask.status?.id === 'sc901303007954_qiBEkmhZ',
+                      'bg-green-8 text-white':
+                        selectedTask.status?.id === 'sc901303007954_cq6ukTMC',
+                      'bg-gray text-black':
+                        selectedTask.status?.id === 'sc901303007954_tGoRmoEs',
+                      'bg-purple-8 text-white mb-05':
+                        selectedTask.status?.id === 'sc901303007954_GXKcfrH2',
+                      'bg-orange-8 text-white':
+                        selectedTask.status?.id === 'sc901303007954_IFDVes5B'
+                    }"
+                  >
+                    {{ selectedTask.status?.id ? $t(`status.${selectedTask.status?.id}`) : 'Aberto' }}
+                  </q-chip>
+                </div>
+                <span v-if="selectedTask.tags && selectedTask.tags.length > 0">
+                  <span v-for="tag in selectedTask.tags" :key="tag.name">
                     <q-chip
-                      size="12px"
-                      :class="{
-                        'bg-orange-8 text-white':
-                          selectedTask.status?.id === 'sc901303007954_qiBEkmhZ',
-                        'bg-yellow-8 text-white':
-                          selectedTask.status?.id === 'sc901303007954_VhnEd1ez',
-                        'bg-blue-8 text-white':
-                          selectedTask.status?.id === 'sc901303007954_qiBEkmhZ',
-                        'bg-green-8 text-white':
-                          selectedTask.status?.id === 'sc901303007954_cq6ukTMC',
-                        'bg-gray text-black':
-                          selectedTask.status?.id === 'sc901303007954_tGoRmoEs',
-                        'bg-purple-8 text-white mb-05':
-                          selectedTask.status?.id === 'sc901303007954_GXKcfrH2',
-                        'bg-orange-8 text-white':
-                          selectedTask.status?.id === 'sc901303007954_IFDVes5B'
-                      }"
+                        size="12px"
+                        :class="'bg-blue-8 text-white'"
                     >
-                      {{ selectedTask.status?.id ? $t(`status.${selectedTask.status?.id}`) : 'Aberto' }}
+                      {{ tag.name }}
                     </q-chip>
-                  </div>
-                  <span v-if="selectedTask.tags && selectedTask.tags.length > 0">
-                    <span v-for="tag in selectedTask.tags" :key="tag.name">
-                      <q-chip
-                          size="12px"
-                          :class="'bg-blue-8 text-white'"
-                      >
-                        {{ tag.name }}
-                      </q-chip>
-                    </span>
                   </span>
-                  <span v-else>Vazio</span>
+                </span>
+                <span v-else><div class="mb-05">Vazio</div></span>
+                <div>
+                {{ selectedTask.custom_fields ? 
+                  (selectedTask.custom_fields.find(field => field.id === 'c019146b-e58d-4351-a0e2-23eb2bc94ffd')?.value || 'Não informado') 
+                  : 'Não informado' }} 
+                </div>
               </section>
             </div>
             <div class="flex-js">
@@ -292,6 +297,7 @@
                 <strong class="mb-05"><q-icon name="supervisor_account" class="mr-05"/>Responsáveis</strong>
                 <strong class="mb-05"><q-icon name="flag" class="mr-05"/>Prioridade</strong>
                 <strong class="mb-05"><q-icon name="build" class="mr-05"/>Técnico responsável</strong>
+                <strong class="mb-05"><q-icon name="headset" class="mr-05"/>Equipe suporte</strong>
               </section>
               <section>
                 <div> {{ selectedTask.assignees.map(a => a.username).join(', ') || 'Não informado' }} </div>
@@ -311,21 +317,26 @@
                     {{ selectedTask.priority ? $t(`priority.${selectedTask.priority?.priority}`) : 'Normal' }}
                   </q-chip>
                 </div>
-                <div>
+                <div class="mb-05">
                   {{ selectedTask.custom_fields ? 
                     (selectedTask.custom_fields.find(field => field.id === '74deb46b-1412-4c18-85b3-d029aa21c523')?.value || 'Não informado') 
+                    : 'Não informado' }} 
+                </div>
+                <div>
+                  {{ selectedTask.custom_fields ? 
+                    (selectedTask.custom_fields.find(field => field.id === '05fd1448-a109-48ad-8075-7e478f0c3712')?.type_config.options[selectedTask.custom_fields.find(field => field.id === '05fd1448-a109-48ad-8075-7e478f0c3712')?.value]?.name || 'Não informado') 
                     : 'Não informado' }} 
                 </div>
               </section>
             </div>
           </div>
-          <section class="mt-2 text-grey-8">Descrição</section>
-          <q-scroll-area class="description-div q-pa-md q-mt-md q-border-radius" style="height: 200px;">
+          <section class="mt-3 text-grey-8">Descrição</section>
+          <q-scroll-area class="description-div q-pa-md q-mt-md q-border-radius" style="height: 300px;">
             <div>{{ selectedTask.description }}</div>
           </q-scroll-area>
         </q-card-section>
 
-        <q-card-actions class="justify-center">
+        <q-card-actions class="justify-center mt-2">
           <q-btn push align="between" color="primary" label="Abrir tarefa no ClickUp" icon="open_in_new" @click="openClickUp(selectedTask.url)" />
         </q-card-actions>
       </q-card>
