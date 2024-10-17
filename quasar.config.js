8f7c26ae9902f38ab29e15f2ea9a1f0863bdc56e
Mateus Albiero <mateusalbiero1@gmail.com>
2024-10-17 17:39:21 -0300

    :bulb: configurado mixin e instalado apexcharts

--- quasar.config.js
+++ quasar.config.js
@@ -22,7 +22,7 @@ module.exports = configure(function (/* ctx */) {
     // https://v2.quasar.dev/quasar-cli-vite/boot-files
     boot: [
       
-      'i18n', 'axios',
+      'i18n', 'axios', 'mixin'
     ],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
