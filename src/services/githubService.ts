e08fbdd6bfc58ff21d52adc5d84b20cbcb18f5ad
Mateus Albiero <mateusalbiero1@gmail.com>
2024-10-18 17:55:52 -0300

    :fire: mudanças no serviço do github

--- src/services/githubService.ts
+++ src/services/githubService.ts
@@ -2,23 +2,17 @@ import { boot } from 'quasar/wrappers';
 import axios, { AxiosInstance } from 'axios';
 
 declare module 'vue' {
-  interface ComponentCustomProperties {
-    $axios: AxiosInstance;
-    $api: AxiosInstance;
-  }
+    interface ComponentCustomProperties {
+        $apiGitHub: AxiosInstance;
+    }
 }
 
-
 const apiGitHub = axios.create({
-  //Header com o token disponível no link devido ao GitGuardian: https://app.clickup.com/t/86a4wcjqk
+    baseURL: 'http://localhost:3000/api/github',
 });
 
-
 export default boot(({ app }) => {
-
-  app.config.globalProperties.$axios = axios;
-  app.config.globalProperties.$api = apiGitHub;
-
+    app.config.globalProperties.$apiGitHub = apiGitHub;
 });
 
 export { apiGitHub };
